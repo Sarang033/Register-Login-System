12c88b056fd85a28a52275ec720d41ace8541331
Sarang Sharma <sarangchamp2--4>
2023-10-21 07:09:50 +0530

    created login/register route

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,13 +3,14 @@ const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 const { JsonWebTokenError } = require("jsonwebtoken");
+const validInfo = require("../middleware/validInfo")
 
 // register
-router.post("/register", async (req, res) => {
+router.post("/register",validInfo,async (req, res) => {
   try {
     //1.Destructure the req.body (name,email,password)
     const { name, email, password } = req.body;
-
+ 
     //2.Check if user exists (if user exists then throw error)
     const user = await pool.query("SELECT * FROM users WHERE user_email =$1", [
       email,
@@ -40,5 +41,33 @@ router.post("/register", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// LOGIN ROUTE
+router.post("/login",validInfo,async(req,res)=>{
+  try {
+
+    //1. Destructure the req.body
+    const {email,password} = req.body;
+
+    //2. Check if user doesn't exist (if not then we throw error)
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1",[email]);
+    if(user.rows.length === 0){
+      return res.status(401).json("Password or email is incorrect")
+    }
+
+    //3. Check if incoming password is the same the database password
+    const validPassword =  await bcrypt.compare(password, user.rows[0].user_password);
+    if(!validPassword){
+      return res.status(401).json("Password or Email is incorrect")
+    }
+
+    //4. Give them the jwt token
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({token})
+    
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 module.exports = router;
