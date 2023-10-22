d1b0d5a1c7c144bc1b7eb301a2a365b7a03b89f3
Sarang Sharma <sarangchamp2--4>
2023-10-22 09:15:16 +0530

    client side

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = boolean => {
+    setIsAuthenticated(boolean)
+  }
+
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props}  setAuth={setAuth}/>
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props}  setAuth={setAuth}/>
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props}  setAuth={setAuth}/>
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
